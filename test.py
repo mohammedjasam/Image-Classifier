5e5f478184c78cd7c27e33e24d9c435f1d930cfd
poojakancherla <pknq6@mst.edu>
2017-05-01 01:25:31 -0500

    Sorted the restaurants

--- test.py
+++ test.py
@@ -16,14 +16,20 @@ def extract_ll(la,lo,code):
     elif code == 4:
         return "-"+la+",-"+lo
 
+
+
 """==================================================="""
 """ ENTER THE LATITUDE AND LONGITUDE & ALSO THE RADIUS"""
 """==================================================="""
+### Give the users Latitude and Longitude
+s="43.6532° N, 79.3832° W"
 
-s="36.1699° N, 115.1398° W"
 ### Distance is in Meters
-dist = 100
+dist = 10
 """==================================================="""
+
+
+
 dist = dist/1000
 la,lo = s.split(',')
 if la[-1]=='N':
@@ -36,20 +42,24 @@ else:
         location=extract_ll(la,lo,3)
     else:
         location=extract_ll(la,lo,4)
-# extract_ll()
+
 lat,lon = location.split(",")
 
+
+
+
+
+"=============================================Code Begins========================================================"
 a=(float(lat),float(lon))
-# print(a)
 
 aLa = str(lat)
 aLo = str(lon)
-# aLa='33.4255'
-# aLo='-111.9400'
-# a = (33.4255,-111.9400)
 
 columns = defaultdict(list)
 
+
+
+### Extracting all the latitudes and longitudes in business dataset for calulating similarity
 with open('yelp_business1.csv') as f:
     reader = csv.DictReader(f)
     for row in reader:
@@ -67,41 +77,47 @@ la={}
 lo={}
 
 
-# print(columns['latitude'][2][0:2])
-# print(len(columns['latitude']))
-
-### Indexes the Latitudes for better search
+### Indexes the Latitudes for better search and for reducing the time complexity
 ind = {}
 for x in range(len(columns['latitude'])):
     try:
         ind[columns['latitude'][x][0:2]].append((columns['latitude'][x],columns['longitude'][x]))
     except:
         ind[columns['latitude'][x][0:2]] = [(columns['latitude'][x],columns['longitude'][x])]
-# print(ind)
-
-# print(aLa[0:2])
-target = ind[aLa[0:2]]
-# print(len(target))
-
-# print(target[0])
 
 
+
+
+target = ind[aLa[0:2]]     ### Time complexity is reduced from n2 to n
+
+
+
+
+
+### Finding out the Euclidean Distance between the User and the Restaurants
 for x in range(len(target)):
     try:
         dst = distance.euclidean(a,(float(target[x][0]),float(target[x][1])))
-        if dst<=dist:   ### Specify the distance in KM
-            # print(x,dst)   ### Returns the id number and the distance between the locations
+        if dst<=dist:
             NearByLocations.append((target[x][0],target[x][1]))
     except:
         pass
-# '''### Contains all the Restaurants near a 1000m radius'''
-# print(NearByLocations)
 
-### Dict of (Lat:Long)
-# print(dict(NearByLocations))
+
+
+
+
+
+'''### Contains all the Restaurants near a earlier defined radius'''
 NearByLocations=dict(NearByLocations)
 
+
+
+
+
+
 fail=0
+troll=[]
 business_id=[]  ## Used to store the restaurant business_id!
 with open('yelp_business1.csv') as f:
     for i,line in enumerate(f):
@@ -111,54 +127,76 @@ with open('yelp_business1.csv') as f:
             line = line.split(",")
             try:
                 if NearByLocations[line[1]]:
+                    troll.append((line[4],(line[0],[line[3],line[4],line[5],line[1],line[2]])))
+
                     #                    business_id,Name,Rating,City,Latitude,Longitude
                     business_id.append((line[0],[line[3],line[4],line[5],line[1],line[2]]))
             except:
                 fail=1
 
-business_id = business_id[0:10]
+
+
+### No nearby locations!
 if fail==1 and len(business_id)==0:
-    print("Sorry, no restaurants nearby!\n")
+    if dist*1000 == 1 or dist*1000 == 0:
+        print("No restaurants where you're standing, please increase the distance! :)\n")
+    else:
+        print("Sorry, no restaurants nearby!\n")
+
+
+
+
+
+
 business_id = dict(business_id)
-# for x in range(5):
-# print(business_id)
+finalRes = {}   ### Used to store the Final Outputs!!!
 
 
 
-finalRes = {}
+
 
 ### Checking the sentiment for the restaurants!
 with open('yelp_reviews.csv') as freviews:
-    # print("City, Restaurant, Rating, Latitude, Longitude")#, business_id, latitude, longitude")
     for i,line in enumerate(freviews):
         if i==0:
             pass
         line = line.split(",")
-        # print(line[1])
 
         if line[1] in business_id.keys():
-            if int(line[7])>0: ## Checks for sentiment
-                if float(business_id[line[1]][1])>3:
-                    finalRes[business_id[line[1]][0]]=(business_id[line[1]][2], business_id[line[1]][0], business_id[line[1]][1], business_id[line[1]][3],business_id[line[1]][4])
-                # print(business_id[line[1]][2], business_id[line[1]][0], business_id[line[1]][1], business_id[line[1]][3],business_id[line[1]][4])#, line[0], line[1])#, line[7])
-        # if line[1] in
-# finalRes = list(set(finalRes))
+            if int(line[7])>0: ## Checks if the sentiment is positive or not!
+                try:
+                    if float(business_id[line[1]][1])>3: ## Average rating of the restaurant!
 
-# print(finalRes)
+                        finalRes[business_id[line[1]][0]]=(business_id[line[1]][2], business_id[line[1]][0], business_id[line[1]][1], business_id[line[1]][3],business_id[line[1]][4])
+                except:
+                    pass
 
-# print (l[0].ljust(10), l[1].ljust(30), l[2].ljust(20))
-# print("{:<8} {:<15} {:<10}".format('City','Restaurant','Rating'))
-print("City".ljust(10), "Restaurant".ljust(30), "Rating".ljust(20))
-print("-------------------------------------------------")
+
+
+### Final outpust!!!
+SortedOutputs = []
+if not(fail==1 and len(business_id)==0):
+    print("City".ljust(20), "Restaurant".ljust(65), "Rating".ljust(30))
+    print("______________________________________________________________________________________________")
+cntr=0
 for k, v in finalRes.items():
     l = v
-    print (l[0].ljust(10), l[1].ljust(30), l[2].ljust(20))
+    SortedOutputs.append((l[2],[l[0],l[1],l[2]]))
+SortedOutputs = sorted(SortedOutputs,reverse=True)
+SortedOutputs = SortedOutputs [0:20]
+OP = []
+for i, v in SortedOutputs:
+    OP.append(v)
 
-    # print ("{:<8} {:<15} {:<10}".format(l[0], l[1], l[2]))
-# for x in finalRes.values():
-#     print(x)
+
+### Displaying the Sorted results with respect to Ratings!
+for l in OP:
+    print (l[0].ljust(20), l[1].ljust(65), l[2].ljust(30))
 
 
+
+
+""" TESTING PHASE """
 ### Finds out the categories in the restaurants
 # main=[]
 #
