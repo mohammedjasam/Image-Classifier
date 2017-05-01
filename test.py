b77211e76c5c6b02fb3b8e9df82053faa1e4dde3
poojakancherla <pknq6@mst.edu>
2017-04-30 23:11:56 -0500

    Working on Sentiment Analysis

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ latlon=[]
 longi=[]
 ctr=0
 
-locs=[]
+NearByLocations=[]
 
 la={}
 lo={}
@@ -51,77 +51,70 @@ for x in range(len(target)):
         dst = distance.euclidean(a,(float(target[x][0]),float(target[x][1])))
         if dst<=0.1:
             # print(x,dst)   ### Returns the id number and the distance between the locations
-            locs.append((target[x][0],target[x][1]))
+            NearByLocations.append((target[x][0],target[x][1]))
     except:
         pass
-
-print(locs)
-
-
-
-
 # '''### Contains all the Restaurants near a 1000m radius'''
-# # print(locs)
-# print()
-# LatLong={}
-# for x in locs:
-#     LatLong[x[0]]=x[1]
-# print(LatLong)
-# business_id=[]
-# with open('yelp_business1.csv') as f:
-#     for i,line in enumerate(f):
-#         if i==0:
-#             pass
-#         else:
-#             line = line.split(",")
+# print(NearByLocations)
+
+### Dict of (Lat:Long)
+# print(dict(NearByLocations))
+NearByLocations=dict(NearByLocations)
+
+fail=0
+business_id=[]  ## Used to store the restaurant business_id!
+with open('yelp_business1.csv') as f:
+    for i,line in enumerate(f):
+        if i==0:
+            pass
+        else:
+            line = line.split(",")
+            try:
+                if NearByLocations[line[1]]:
+                    business_id.append((line[0],[line[1],line[2]]))
+            except:
+                fail=1
+
+business_id = business_id[0:10]
+if fail==1 and len(business_id)==0:
+    print("Sorry, no restaurants nearby!\n")
+business_id = dict(business_id)
+# for x in range(5):
+# print(business_id)
+
+
+
+
+
+### Checking the sentiment for the restaurants!
+with open('yelp_reviews.csv') as freviews:
+    print("user_id, business_id, latitude, longitude")
+    for i,line in enumerate(freviews):
+        if i==0:
+            pass
+        line = line.split(",")
+        # print(line[1])
+
+        if line[1] in business_id.keys():
+            if int(line[7])>20: ## Checks for sentiment
+
+                print(line[0],line[1], line[7])#, business_id[line[1]])
+        # if line[1] in
+
+
+
+### Finds out the categories in the restaurants
+# main=[]
 #
-#             try:
-#                 if LatLong[line[1]]:
-#                     # print(line[1],line[2])
-#                     # print(LatLong[line[1]],line[2])
-#                     # print()
-#                     if LatLong[line[1]]==line[2]:
-#                         # print(line[1],line[2])
-#                         print("yes")
+# for x in columns['categories']:
+#     jj=ast.literal_eval(x)
+#     if 'Restaurants' in jj:
+#         l.append(jj)
 #
-#                     # business_id.append(line[])
-#             except:
-#                 # print("No such location found")
-#                 pass
-#         # print(line[1],line[2])
-#         # if line[1] in locs[]
-#         # if i<=5:
-#         #     print(line.split(','))# if i<=5 else break)
-# # for x in columns['latitude']:
-# #     for y in columns['longitude']:
-# #         dst = distance.euclidean(a,(float(x),float(y)))
-# #         if dst<=0.3:
-# #             locs.append((x,y))
-# #
-# # print(locs)
+# for x in l:
+#     for y in x:
+#         main.append(y)
 #
-# # print("HI")
-# #
-# # for x in columns['categories']:
-# #     jj=ast.literal_eval(x)
-# #     if 'Restaurants' in jj:
-# #         l.append(jj)
-# #
-# # main=[]
-# #
-# # for x in l:
-# #     for y in x:
-# #         main.append(y)
-# #
-# # main=list(set(main))
-# #
+# main=list(set(main))
 #
-#
-#
-#
-#
-#
-#
-#
-#
-# # print(main)
+# print(main)
